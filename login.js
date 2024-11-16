6aa877e09e3e51741782cd2fad3989ce8a7bccf7
1234Ganesh <ganeshkonda346@gmail.com>
2024-11-16 20:34:09 +0530

    hhh

--- login.js
+++ login.js
@@ -3,7 +3,10 @@ console.log("login function")
 
 let email = "ganesh346@gmail.com";
 
-let email='amarpalli@gmail.com'
+let email = "rushi@gmail.com";
+
+
+let email="mahesh@gmail.com";
 
 console.log("bug fixed");
 }
